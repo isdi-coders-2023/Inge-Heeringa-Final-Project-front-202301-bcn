3f2ee8980433b7c023bc44c37dfd8d9b0751a047
Inge Heeringa <inge.heeringa@gmail.com>
2023-03-08 14:27:49 +0100

    Update test loginForm component with Angular Testing Library

--- src/app/components/login-form/login-form.component.spec.ts
+++ src/app/components/login-form/login-form.component.spec.ts
@@ -1,22 +1,65 @@
-import { ComponentFixture, TestBed } from "@angular/core/testing";
-
+import { render, screen } from "@testing-library/angular";
+import "@testing-library/jest-dom";
 import { LoginFormComponent } from "./login-form.component";
 
-describe("LoginFormComponent", () => {
-  let component: LoginFormComponent;
-  let fixture: ComponentFixture<LoginFormComponent>;
+describe("Given a LoginForm component", () => {
+  describe("When rendered", () => {
+    test("Then it should show a form", async () => {
+      await render(LoginFormComponent);
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [LoginFormComponent],
-    }).compileComponents();
+      const form = screen.getByRole("form");
 
-    fixture = TestBed.createComponent(LoginFormComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+      expect(form).toBeInTheDocument();
+    });
 
-  it("should create", () => {
-    expect(component).toBeTruthy();
+    test("Then it should show the title 'Log in' in a heading", async () => {
+      const title = /log in/i;
+
+      await render(LoginFormComponent);
+
+      const loginTitle = screen.getByRole("heading", { name: title });
+
+      expect(loginTitle).toBeInTheDocument();
+    });
+
+    test("Then it should show an input field for an email address", async () => {
+      const labelText = /email/i;
+
+      await render(LoginFormComponent);
+
+      const emailInput = screen.getByLabelText(labelText);
+
+      expect(emailInput).toBeInTheDocument();
+    });
+
+    test("Then it should show an input field for a password", async () => {
+      const labelText = /password/i;
+
+      await render(LoginFormComponent);
+
+      const passwordInput = screen.getByLabelText(labelText);
+
+      expect(passwordInput).toBeInTheDocument();
+    });
+
+    test("Then it should show a submit button with the text 'Log in'", async () => {
+      const buttonText = "Log in";
+
+      await render(LoginFormComponent);
+
+      const submitButton = screen.getByRole("button", { name: buttonText });
+
+      expect(submitButton).toBeInTheDocument();
+    });
+
+    test("Then it should show a redirect link to the register page", async () => {
+      const linkText = /not a member yet\? sign up/i;
+
+      await render(LoginFormComponent);
+
+      const redirectLink = screen.getByRole("link", { name: linkText });
+
+      expect(redirectLink).toBeInTheDocument();
+    });
   });
 });
