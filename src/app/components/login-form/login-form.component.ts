b2a20e5b1d641511ea1ecf3ba18f6deff93d4daf
Inge Heeringa <inge.heeringa@gmail.com>
2023-03-08 17:26:15 +0100

    Add form validation to loginForm component

--- src/app/components/login-form/login-form.component.ts
+++ src/app/components/login-form/login-form.component.ts
@@ -1,5 +1,5 @@
 import { Component } from "@angular/core";
-import { FormControl } from "@angular/forms";
+import { FormBuilder, Validators } from "@angular/forms";
 
 @Component({
   selector: "app-login-form",
@@ -7,6 +7,44 @@ import { FormControl } from "@angular/forms";
   styleUrls: ["./login-form.component.scss"],
 })
 export class LoginFormComponent {
-  emailFormControl = new FormControl();
-  passwordFormControl = new FormControl();
+  loginForm = this.fb.group({
+    email: [
+      "",
+      [
+        Validators.required,
+        Validators.email,
+        Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$"),
+      ],
+    ],
+    password: [
+      "",
+      [Validators.required, Validators.minLength(8), Validators.maxLength(20)],
+    ],
+  });
+
+  constructor(private fb: FormBuilder) {}
+
+  getErrorMessageEmail() {
+    if (this.loginForm.controls["email"].hasError("required")) {
+      return "You must enter an email address";
+    }
+
+    return this.loginForm.controls["email"].hasError("email" || "pattern")
+      ? "Not a valid email"
+      : "";
+  }
+
+  getErrorMessagePassword() {
+    if (this.loginForm.controls["password"].hasError("required")) {
+      return "You must enter a password";
+    }
+
+    if (this.loginForm.controls["password"].hasError("minlength")) {
+      return "Password must be at least 8 characters long";
+    }
+
+    return this.loginForm.controls["password"].hasError("maxlength")
+      ? "Maximum password length is 20 characters"
+      : "";
+  }
 }
