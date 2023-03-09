490d386580374624c85fb85bdb1f656ed3058a4c
Inge Heeringa <inge.heeringa@gmail.com>
2023-03-09 10:50:43 +0100

    Configure environments

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["xo", "prettier"],
+  overrides: [
+    {
+      extends: ["xo-typescript", "prettier"],
+      files: ["*.ts"],
+      rules: {
+        "@typescript-eslint/consistent-type-imports": "off",
+        "@typescript-eslint/consistent-type-definitions": [
+          "error",
+          "interface",
+        ],
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {
+    "new-cap": [
+      "error",
+      { capIsNewExceptions: ["NgModule", "Component", "Injectable"] },
+    ],
+  },
+};
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+  apiUrl: "https://inge-heeringa-final-project-202301-bcn.onrender.com/",
+  paths: {
+    users: "/users",
+    login: "/login",
+  },
+};
