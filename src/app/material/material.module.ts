d1acf1d0f1f30f509efe768e3b2cf4582f811b2f
Inge Heeringa <inge.heeringa@gmail.com>
2023-03-10 12:15:23 +0100

    Add styles to Navigation component

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from "@angular/core";
 import { MatButtonModule } from "@angular/material/button";
 import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatInputModule } from "@angular/material/input";
-import { MatIcon, MatIconModule } from "@angular/material/icon";
+import { MatIconModule } from "@angular/material/icon";
 
 @NgModule({
   exports: [MatButtonModule, MatInputModule, MatFormFieldModule, MatIconModule],
