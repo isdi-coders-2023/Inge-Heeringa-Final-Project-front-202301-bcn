e5b0c953057f2eec9c4288af0d9ceb6bc552f6e9
Inge Heeringa <inge.heeringa@gmail.com>
2023-03-10 09:26:27 +0100

    Create Navigation Component structure

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -2,8 +2,9 @@ import { NgModule } from "@angular/core";
 import { MatButtonModule } from "@angular/material/button";
 import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatInputModule } from "@angular/material/input";
+import { MatIcon, MatIconModule } from "@angular/material/icon";
 
 @NgModule({
-  exports: [MatButtonModule, MatInputModule, MatFormFieldModule],
+  exports: [MatButtonModule, MatInputModule, MatFormFieldModule, MatIconModule],
 })
 export class MaterialModule {}
