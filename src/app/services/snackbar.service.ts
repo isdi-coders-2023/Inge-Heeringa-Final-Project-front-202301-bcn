681078834ab0c9a7f2113809a7f57bfaba1cc198
Inge Heeringa <inge.heeringa@gmail.com>
2023-03-10 22:57:37 +0100

    Create Snackbar Service

---
+++ src/app/services/snackbar.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from "@angular/core";
+import {
+  MatSnackBar,
+  MatSnackBarVerticalPosition,
+} from "@angular/material/snack-bar";
+
+@Injectable({
+  providedIn: "root",
+})
+export class SnackBarService {
+  verticalPosition: MatSnackBarVerticalPosition = "top";
+
+  constructor(private readonly snackBar: MatSnackBar) {}
+
+  openSnackBar(message: string) {
+    this.snackBar.open(message, "Close", {
+      verticalPosition: this.verticalPosition,
+    });
+  }
+}
