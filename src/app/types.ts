ea733302fd2a908a71c2410a9050eb50ed5cd074
Inge Heeringa <inge.heeringa@gmail.com>
2023-03-10 23:02:49 +0100

    Refactor User Feature

--- src/app/types.ts
+++ src/app/types.ts
@@ -1,19 +1,5 @@
 import { JwtPayload } from "jwt-decode";
 
-export interface User {
-  email: string;
-  token: string;
-}
-
-export interface UserState extends User {
-  isLogged: boolean;
-}
-
-export interface UserCredentials {
-  email: string;
-  password: string;
-}
-
 export interface CustomTokenPayload extends JwtPayload {
   email: string;
 }
