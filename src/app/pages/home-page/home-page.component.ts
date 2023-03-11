18dd26d9c3f1dba2629d6bd1488673024c090aeb
Inge Heeringa <inge.heeringa@gmail.com>
2023-03-11 13:28:09 +0100

    Add conditional rendering of home page buttons based on isLogged status

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,8 +1,23 @@
 import { Component } from "@angular/core";
+import { Store } from "@ngrx/store";
+import { Observable } from "rxjs";
+import { UserService } from "src/app/services/user/user.service";
+import { selectIsLogged } from "src/app/store/user/user.reducer";
 
 @Component({
   selector: "app-home-page",
   templateUrl: "./home-page.component.html",
   styleUrls: ["./home-page.component.scss"],
 })
-export class HomePageComponent {}
+export class HomePageComponent {
+  isLogged$: Observable<boolean> = this.store.select(selectIsLogged);
+
+  constructor(
+    private readonly store: Store,
+    private readonly userService: UserService
+  ) {}
+
+  logoutUser() {
+    this.userService.logout();
+  }
+}
