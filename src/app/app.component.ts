bf0c036586ae5cb5928d49c812f68ae1112bdd90
Inge Heeringa <inge.heeringa@gmail.com>
2023-03-11 17:25:24 +0100

    Manage login action based on token in storage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,36 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { Store } from "@ngrx/store";
+import decode from "jwt-decode";
 import { Observable } from "rxjs";
 import { UiService } from "./services/ui/ui.service";
+import { loginUser } from "./store/user/user.actions";
+import { CustomTokenPayload } from "./types";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   isLoading$: Observable<boolean>;
 
-  constructor(private readonly uiService: UiService) {
+  constructor(
+    private readonly uiService: UiService,
+    private readonly store: Store
+  ) {
     this.isLoading$ = this.uiService.getIsLoading();
+  }
+
+  ngOnInit(): void {
+    this.checkIsLogged();
+  }
+
+  private checkIsLogged() {
+    const token = localStorage.getItem("token");
+
+    if (token) {
+      const { email } = decode(token) as CustomTokenPayload;
+
+      this.store.dispatch(loginUser({ payload: { email, token } }));
+    }
   }
 }
