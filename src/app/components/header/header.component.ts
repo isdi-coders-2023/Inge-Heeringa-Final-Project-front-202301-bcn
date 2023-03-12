e17f9ab8b158401705db5e3f51fc785ec2a5edaa
Inge Heeringa <inge.heeringa@gmail.com>
2023-03-12 20:20:47 +0100

    Add conditional rendering of CTAs based on isLogged and current path

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,8 +1,18 @@
 import { Component } from "@angular/core";
+import { Router } from "@angular/router";
+import { Store } from "@ngrx/store";
+import { Observable } from "rxjs";
+import { selectIsLogged } from "src/app/store/user/user.reducer";
+import { environment } from "../../../environments/environment";
 
 @Component({
   selector: "app-header",
   templateUrl: "./header.component.html",
   styleUrls: ["./header.component.scss"],
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  isLogged$: Observable<boolean> = this.store.select(selectIsLogged);
+  urls = ["/", "/users/login", "/users/register"];
+
+  constructor(public store: Store, public router: Router) {}
+}
