b77a1889f11b8cda0c9570a5729049034139d09e
Inge Heeringa <inge.heeringa@gmail.com>
2023-03-12 12:39:39 +0100

    Add registerForm group with validators

--- src/app/components/register-form/register-form.component.ts
+++ src/app/components/register-form/register-form.component.ts
@@ -1,8 +1,30 @@
 import { Component } from "@angular/core";
+import { FormBuilder, Validators } from "@angular/forms";
 
 @Component({
   selector: "app-register-form",
   templateUrl: "./register-form.component.html",
   styleUrls: ["./register-form.component.scss"],
 })
-export class RegisterFormComponent {}
+export class RegisterFormComponent {
+  registerForm = this.fb.group({
+    username: [
+      "",
+      [Validators.required, Validators.minLength(3), Validators.maxLength(12)],
+    ],
+    email: [
+      "",
+      [
+        Validators.required,
+        Validators.email,
+        Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$"),
+      ],
+    ],
+    password: [
+      "",
+      [Validators.required, Validators.minLength(8), Validators.maxLength(20)],
+    ],
+  });
+
+  constructor(private readonly fb: FormBuilder) {}
+}
