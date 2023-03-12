23ced83ee0ad154a1c4a291e56e6d0c9c61adb34
Inge Heeringa <inge.heeringa@gmail.com>
2023-03-12 12:14:11 +0100

    Add route to register page to router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,17 @@ import { RouterModule, Routes } from "@angular/router";
 import { HomePageComponent } from "./pages/home-page/home-page.component";
 import { LoginPageComponent } from "./pages/login-page/login-page.component";
 import { NotFoundPageComponent } from "./pages/not-found-page/not-found-page.component";
+import { RegisterPageComponent } from "./pages/register-page/register-page.component";
 
 const routes: Routes = [
   { path: "", component: HomePageComponent },
   {
     path: "users/login",
     component: LoginPageComponent,
+  },
+  {
+    path: "users/register",
+    component: RegisterPageComponent,
   },
   { path: "**", component: NotFoundPageComponent },
 ];
